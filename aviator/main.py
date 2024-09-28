3b330a32dca0217fefc6b2d58875f171e965b5b9
Brian kimany <briankimani328@gmail.com>
2024-09-28 13:22:52 +0300

    Aviator api ui

---
+++ aviator/main.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from st_utils import fetch_data , generate_scatter_plot , generate_heatmap , generate_histogram , transform_to_df ,get_months_available
+
+
+st.set_page_config(layout="wide")
+st.title("Odds Analysis")
+
+col1, col2, col3 = st.columns(3)
+
+url = "https://kimany.pythonanywhere.com/data"  # Replace with your actual URL
+
+
+with col1:
+    # Input fields for parameters
+    auth_key = st.text_input("Enter Auth Key")
+    data_type = st.selectbox("Select Data Type", options=["Daily", "Monthly", "Weekly", "Custom Range"])
+
+    if data_type == "Daily":
+        daily = st.date_input("Select a Date")
+        params = {'daily': daily.strftime("%Y-%m-%d")}
+    elif data_type == "Monthly":
+        response = get_months_available("https://kimany.pythonanywhere.com" ,auth_key)
+        if response == 200:
+                
+            available_monthly_data = list(response.json()['data'].keys())
+            available_years = set([int(i.split('-')[0]) for i in response.json()['data'].values()])
+
+            year = st.selectbox("Select a year", options=available_years)
+            monthly = st.selectbox("Select a Month", options=available_monthly_data)
+            month_to_int = {
+                "January": 1,
+                "February": 2,
+                "March": 3,
+                "April": 4,
+                "May": 5,
+                "June": 6,
+                "July": 7,
+                "August": 8,
+                "September": 9,
+                "October": 10,
+                "November": 11,
+                "December": 12
+            }
+            params = {'monthly': f"{year}-{month_to_int[monthly]:02d}"}
+        else:
+            st.error("Unauthorized acces")
+            params= None
+    elif data_type == "Weekly":
+        weekly = st.date_input("Select a Week")
+        params = {'weekly': weekly.strftime("%Y-%m")}
+    else:
+        start_date = st.date_input("Select a Start Date")
+        end_date = st.date_input("Select an End Date")
+        params = {'start_date': start_date.strftime("%Y-%m-%d"), 'end_date': end_date.strftime("%Y-%m-%d")}
+
+    if st.button("Fetch Data"):
+        if auth_key and data_type:
+            data = None
+            if params:
+                data = fetch_data(url, auth_key, params)
+
+            if data is not None:
+                st.session_state.data = data
+                st.warning("Fetching data ,please wait.")
+                st.session_state.df=transform_to_df(data)
+                st.success("Data fetched successfully!")
+        else:
+            st.warning("Please fill in all fields.")
+
+with col2:
+    # Check if data is available in session state
+    if 'data' in st.session_state:
+            df = st.session_state.df
+            with st.expander('TABLE'):
+                st.table(df[['date','time', 'value']])
+
+with col3:
+    if 'data' in st.session_state:
+        df = st.session_state.df
+        if st.button("Show Heatmap"):
+            heatmap_fig = generate_heatmap(df)
+            st.pyplot(heatmap_fig)
+
+        
+        if st.button("Show Scatter Plot"):
+            scatter_fig = generate_scatter_plot(df)
+            st.pyplot(scatter_fig)
+
+        if st.button("Show Histogram"):
+            histogram_fig = generate_histogram(df)
+            st.pyplot(histogram_fig)
---
+++ aviator/st_utils.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+import streamlit as st
+
+def fetch_data(url, auth_key, params):
+    headers = {
+        'Authorization': f'Bearer {auth_key}'  # Include the Bearer token in the headers
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()  # Assuming the response is in JSON format
+    if response.status_code == 401:
+        st.error("Unauthorized access")
+
+    else:
+        st.error("Failed to fetch data from the server.")
+    return None
+
+def get_months_available(url , auth_key):
+	headers = {
+	'Authorization': f'Bearer {auth_key}'  # Include the Bearer token in the headers
+	}
+	url = url.strip("/") +f"/available_months"
+	response = requests.get(url , params= {"year":2024} , headers=headers)
+ 
+    
+	return response
+
+def format_time(time, data_date):
+    try:
+        if data_date:
+            time = data_date.strip() + "-" + time.strip()
+        return datetime.strptime(time, "%Y-%m-%d-%H:%M:%S")
+    except Exception as e:
+        return None
+
+def create_df(df_list, data_date):
+    df = pd.DataFrame(df_list)[['time', 'value']]
+    df['datetime'] = df['time'].map(lambda x: format_time(time=x, data_date=data_date))
+    df['day'] = df['datetime'].map(lambda x: x.day)
+    df['hour'] = df['datetime'].map(lambda x: x.hour)
+    df['minute'] = df['datetime'].map(lambda x: x.minute)
+    df['second'] = df['datetime'].map(lambda x: x.second)
+
+    return df
+
+def generate_heatmap(df):
+    pivot_table = df.pivot_table(values='value', index='day', columns='hour', aggfunc='mean')
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(pivot_table, cmap='viridis', annot=True, fmt=".2f")
+    plt.title('Heatmap of Average 0-Odd Value by Day and Hour')
+    plt.xlabel('Hour')
+    plt.ylabel('Day')
+    return plt
+
+def generate_scatter_plot(df):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['hour'], df['value'], alpha=0.5)
+    plt.title('0-Odd Value vs. Hour of Day')
+    plt.xlabel('Hour')
+    plt.ylabel('0-Odd Value')
+    return plt
+
+def generate_histogram(df):
+    plt.figure(figsize=(10, 6))
+    plt.hist(df['value'], bins=30, edgecolor='black')
+    plt.title('Distribution of 0-Odd Values')
+    plt.xlabel('0-Odd Value')
+    plt.ylabel('Frequency')
+    return plt
+
+def transform_to_df(response):
+    data = response['data']
+    
+    # Initialize an empty list to store the DataFrames
+    dfs = []
+    
+    # Check the structure of the data
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                # Test case 5: Get weekly data for a month
+                for sub_key, sub_value in value.items():
+                    df = pd.DataFrame(sub_value)
+                    df['date'] = sub_key
+                    dfs.append(df)
+            elif isinstance(value, list):
+                df = pd.DataFrame(value)
+                df['date'] = key
+                dfs.append(df)
+            else:
+                for sub_key, sub_value in value.items():
+                    for sub_sub_key, sub_sub_value in sub_value.items():
+                        
+                        df = pd.DataFrame(sub_sub_value)
+                        df['date'] = sub_sub_key
+                        dfs.append(df)
+    
+    
+    
+    df = pd.concat(dfs, ignore_index=True)
+    df = df[['value', 'time', 'date']]
+    
+    df['date'] = df['date'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d").date())
+    df['value'] = df['value'].astype(float)
+    df['time'] = df['time'].apply(lambda x: datetime.strptime(x, "%H:%M:%S").time())
+
+    df['day'] = df['date'].map(lambda x: x.day)
+    df['hour'] = df['time'].map(lambda x: x.hour)
+    df['minute'] = df['time'].map(lambda x: x.minute)
+    df['second'] = df['time'].map(lambda x: x.second)
+    
+    return df
